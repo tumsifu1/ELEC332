87c464d4b25869081b9d58af3ab725ce988e954a
tumsifu1 <90883982+tumsifu1@users.noreply.github.com>
2023-04-06 14:41:48 -0400

    first commit

---
+++ add_customer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Add Customer</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Add Customer
---
+++ orders_summary.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Orders Summary</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Orders Summary</h1>
+    <!-- Add PHP code and HTML table for displaying orders summary -->
+    <a href="restaurant.php" class="btn">Back to Home</a>
+  </div>
+</body>
+</html>
